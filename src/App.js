339aea638e63a0b8f106521daa205526c7157603
Growthack90 <hackmaker90@outlook.it>
2021-05-12 16:29:25 +0200

    aggiunti componenti home e chisono

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import Template from "./components/Template.js";
+import Home from "./components/Home.js";
+import ChiSono from "./components/ChiSono.js";
 class App extends Component {
 
   constructor(props){
@@ -9,7 +11,12 @@ class App extends Component {
   render() {
     return(
       <BrowserRouter>
-        Prova
+        <Template>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/chi-sono" component={ChiSono}/>
+          </Switch>
+        </Template>
       </BrowserRouter>
     );
   }
