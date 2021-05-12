faaac77f8cac75f74963e010eb3d3242ebece514
Growthack90 <hackmaker90@outlook.it>
2021-05-12 16:10:12 +0200

    creazione primi componenti

---
+++ components/Template.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default class Template extends Component {
+
+    constructor(props){
+      super(props);
+    }
+    render() {
+      return(
+          <div>
+              <Header />
+              {this.props.children}
+              <Footer />
+          </div>
+      );
+    }
+  }
